c3e3576243a2b009e55cc8bf100bbf0c5bb27871
leedi88 <leedi@delightroom.com>
2023-06-08 16:10:57 +0900

    Provider 구현

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mafia_game/widgets/change_member_count_widget.dart';
 import 'package:mafia_game/widgets/change_mode_widget.dart';
 import 'package:mafia_game/widgets/select_theme_grid_widget.dart';
-import 'dart:developer';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,58 +14,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ChangeNotifierProvider(
+            create: (BuildContext context) => HomeViewModel(),
+            child: MyHomePage()));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  late HomeViewModel _viewModel;
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  onChangeModeTap() {
-    log("onChangeModeTap");
-  }
-
-  onPlusMemberCount() {
-    log("onPlusMemberCount");
-  }
-
-  onMinusMemberCount() {
-    log("onMinusMemberCount");
-  }
-
-  onPlusLiarCount() {
-    log("onPlusLiarCount");
-  }
-
-  onMinusLiarCount() {
-    log("onMinusLiarCount");
-  }
-
-  onSelectTheme() {
-    log("onSelectTheme");
-  }
-
+  MyHomePage({super.key});
   @override
   Widget build(BuildContext context) {
+    _viewModel = Provider.of<HomeViewModel>(context);
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
             children: [
               ChangeModeWidget(
-                onPressed: onChangeModeTap,
+                onPressed: () {
+                  _viewModel.changeMode();
+                },
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 40),
@@ -73,25 +48,71 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     ChangeMemberCountWidget(
-                      desc: "총 n명",
-                      onPressedPlus: onPlusMemberCount,
-                      onPressedMinus: onMinusMemberCount,
+                      desc:
+                          "${Provider.of<HomeViewModel>(context).memberCount}명",
+                      onPressedPlus: () {
+                        _viewModel.plusMemberCount();
+                      },
+                      onPressedMinus: () {
+                        _viewModel.minusMemberCount();
+                      },
                     ),
                     ChangeMemberCountWidget(
-                      desc: "라이어 n명",
-                      onPressedPlus: onPlusLiarCount,
-                      onPressedMinus: onMinusLiarCount,
+                      desc: "${Provider.of<HomeViewModel>(context).lierCount}명",
+                      onPressedPlus: () {
+                        _viewModel.plusLiarCount();
+                      },
+                      onPressedMinus: () {
+                        _viewModel.minusLiarCount();
+                      },
                     ),
                   ],
                 ),
               ),
               SelectThemeGridWidget(
-                onSelectTheme: onSelectTheme,
+                onSelectTheme: () {
+                  _viewModel.selectTheme();
+                },
               )
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class HomeViewModel with ChangeNotifier {
+  int _memberCount = 0;
+  int _lierCount = 0;
+  String get memberCount => _memberCount.toString();
+  String get lierCount => _lierCount.toString();
+
+  void changeMode() {
+    notifyListeners();
+  }
+
+  void selectTheme() {
+    notifyListeners();
+  }
+
+  void plusMemberCount() {
+    _memberCount += 1;
+    notifyListeners();
+  }
+
+  void minusMemberCount() {
+    _memberCount -= 1;
+    notifyListeners();
+  }
+
+  void plusLiarCount() {
+    _lierCount += 1;
+    notifyListeners();
+  }
+
+  void minusLiarCount() {
+    _lierCount -= 1;
+    notifyListeners();
   }
 }
